449833d6d2c758484559c0ddaba8b9bafab8af5a
Sinevia <info@sinevia.com>
2018-12-18 07:46:51 +0000

    Update ContactAsAService.php

--- src/ContactAsAService.php
+++ src/ContactAsAService.php
@@ -5,7 +5,7 @@ namespace Sinevia;
 class ContactAsAService {
     public static function send($data) {
         $serviceUrl = "https://eu-gb.functions.cloud.ibm.com/api/v1/web/sinevia_live/default/contact-as-a-service-mail-save.json";
-        $finalUrl = $serviceUrl . '?' . html_build_query($data);
+        $finalUrl = $serviceUrl . '?' . http_build_query($data);
         $responseString = file_get_contents($finalUrl);
         $response = json_decode($responseString,true);
         return $response;
