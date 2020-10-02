2594eb814b316ea9519106145f15d4949cb490e9
Milan Lesichkov <lesichkovm@gmail.com>
2020-10-02 04:42:15 +0100

    Changed the API URL with the new service

--- src/ContactAsAService.php
+++ src/ContactAsAService.php
@@ -4,7 +4,7 @@ namespace Sinevia;
 
 class ContactAsAService {
     public static function send($data) {
-        $serviceUrl = "https://eu-gb.functions.cloud.ibm.com/api/v1/web/sinevia_live/default/contact-as-a-service-mail-save.json";
+        $serviceUrl = "https://contact-as-a-service-uodwjwllpq-ez.a.run.app/email-save";
         $finalUrl = $serviceUrl . '?' . http_build_query($data);
         $responseString = file_get_contents($finalUrl);
         $response = json_decode($responseString,true);
